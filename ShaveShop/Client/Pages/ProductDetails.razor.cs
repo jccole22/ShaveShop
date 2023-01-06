561709e772fabc7d37b7006cf50ebbd54ca0b26e
Jonah <jonahcole@yahoo.com>
2023-01-06 13:25:04 -0800

    Add product page for single product instead of whole list

---
+++ ShaveShop/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShaveShop.Client.Services.ProductService;
+using ShaveShop.Shared;
+
+namespace ShaveShop.Client.Pages
+{
+    public class ProductDetailsBase : ComponentBase
+    {
+        [Inject]
+        protected IProductService ProductService { get; set; }
+
+        protected Product? product = null;
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            product = ProductService.Products.Find(p => p.Id == Id);
+        }
+    }
+}
