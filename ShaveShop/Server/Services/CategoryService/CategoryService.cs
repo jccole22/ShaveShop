18c7285ce2196c73f65b435691be58d122be2da1
Jonah <jonahcole@yahoo.com>
2023-01-24 11:09:20 -0800

    Add category method and implement basic category service for http get

--- ShaveShop/Server/Services/CategoryService/CategoryService.cs
+++ ShaveShop/Server/Services/CategoryService/CategoryService.cs
@@ -1,7 +1,23 @@
-﻿namespace ShaveShop.Server.Services.CategoryService
+﻿using ShaveShop.Server.Data;
+
+namespace ShaveShop.Server.Services.CategoryService
 {
     public class CategoryService : ICategoryService
     {
+        private readonly DataContext _context;
 
+        public CategoryService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
     }
 }
