72f79ef5e4ddfd08d1506ab521339cbcf44d4568
Jonah <jonahcole@yahoo.com>
2023-02-04 18:01:23 -0800

    Add controller for subcategory that calls service class

---
+++ ShaveShop/Server/Controllers/SubCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShaveShop.Server.Services.SubCategoryService;
+
+namespace ShaveShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoryController : ControllerBase
+    {
+        private readonly ISubCategoryService _subCategoryService;
+        public SubCategoryController(ISubCategoryService subCategoryService)
+        {
+            _subCategoryService = subCategoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<SubCategory>>>> GetSubCategories()
+        {
+            var result = await _subCategoryService.GetSubCategoriesAsync();
+            return Ok(result);
+        }
+    }
+}
